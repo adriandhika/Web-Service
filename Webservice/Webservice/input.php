ccae12d76b4941dcdfbd255a134345138ce8c97d
adriandhika <37827160+adriandhika@users.noreply.github.com>
2018-04-01 21:51:04 +0700

    Add files via upload

---
+++ Webservice/Webservice/input.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input Data</title>
+</head>
+<body>
+	
+	<form action="" method="post">
+		Nim<br>
+		<input type="text" name="nim"><br>
+		Nama<br>
+		<input type="text" name="nama"><br>		
+		Progdi<br>
+		<input type="text" name="progdi"><br>
+		<br>
+		<input type="submit" name="submit" value="Save"><br>
+	</form>
+<?php
+include "koneksi.php";
+
+if (isset($_POST['submit'])) {
+	$nim  	= $_POST['nim'];
+	$nama  	=$_POST['nama'];
+	$progdi = $_POST['progdi'];
+	
+	$result = mysqli_query($koneksi, "INSERT INTO siswa(nim,nama,progdi) VALUES('$nim','$nama','$progdi')");
+	$pesan= "";
+	if ($result) {
+		$pesan = "Data Berhasil disimpan";
+		header("location:ClientMahasiswa.php?id=$nim");
+	}else{
+		$pesan ="Data Gagal Disimpan";
+	}
+
+	$response= array('pesan'=>$pesan , 'data'=>$_POST);
+	//
+
+}
+?>
+</body>
+</html>
